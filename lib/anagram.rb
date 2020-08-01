94343dc2127be514cb1f5da5aa6ed8137a390635
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-01 04:27:02 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,13 @@
 class Anagram
   
-  attr_accessor 
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(array)
+    @word.split("").sort
+  end
+    
 end 
